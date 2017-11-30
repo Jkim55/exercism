3ebc86a46f5b299c9f0b46af01045578357c99ca
Ji Kim <jkim55@gmail.com>
2017-11-29 18:46:03 -0700

    clean up leap

--- ecmascript/leap/leap.js
+++ ecmascript/leap/leap.js
@@ -5,5 +5,5 @@ export default function isLeapYear (year){
 const evaluate = {
   is400thYear: function(year){return year % 400 === 0},
   is100thYear: function(year){return year % 100 === 0},
-  is4thYear: function(year){return year % 4 ===0}
+  is4thYear: function(year){return year % 4 === 0}
 }
